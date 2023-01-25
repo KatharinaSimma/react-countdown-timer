6ad6e9ef87bfefc0e765720c64b25af2ee419ece
Katharina Simma <katharina.simma@diebin.at>
2023-01-25 19:06:14 +0100

    Allow user to choose time zone

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import dayjs from 'dayjs/esm';
 import duration from 'dayjs/plugin/duration';
 import isToday from 'dayjs/plugin/isToday';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import tz from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { useEffect, useState } from 'react';
 import events from './events.json';
+import timezones from './timezonesTZ.json';
 
 dayjs.extend(relativeTime);
 dayjs.extend(duration);
 dayjs.extend(isToday);
+dayjs.extend(utc);
+dayjs.extend(tz);
 
 function App() {
-  const now = dayjs();
+  const now = dayjs.utc();
+  const [timezone, setTimezone] = useState();
   const [eventId, setEventId] = useState(0);
-  const [eventDate, setEventDate] = useState(dayjs(events[eventId].date));
+  const [eventDate, setEventDate] = useState(dayjs(events[0].date));
   const [countdown, setCountdown] = useState(
     dayjs.duration(eventDate.diff(now, 'ms')),
   );
+
+  function handleEventSelect(e) {
+    setEventId(e.target.value);
+    const newDate = dayjs(events[e.target.value].date);
+    setEventDate(newDate);
+  }
 
   useEffect(() => {
     if (!eventDate.isToday()) {
@@ -23,24 +35,18 @@ function App() {
         const timeToEventMs = eventDate.diff(now, 'ms');
         setCountdown(dayjs.duration(timeToEventMs));
       }, 1000);
-
       return () => clearInterval(durationInterval);
     }
   }, [eventDate, now]);
 
-  function handleEventSelect(e) {
-    setEventId(e.target.value);
-    setEventDate(dayjs(events[e.target.value].date));
-  }
-
-  // format units
+  // format units for render
   const seconds = countdown.format('s');
   const minutes = countdown.format('m');
   const hours = countdown.format('H');
   const days = Math.floor(countdown.asDays());
   return (
     <>
-      <div style={{ minHeight: 'calc(100vh - 200px)' }}>
+      <div style={{ minHeight: 'calc(100vh - 30vh)' }}>
         <h1>Countdown timer</h1>
         <h2>{events[eventId].name}</h2>
         <p>{eventDate.format('MMMM D YYYY')}</p>
@@ -76,13 +82,6 @@ function App() {
         )}
 
         <br />
-        <div
-          style={{
-            height: '30vh',
-            width: '100vw',
-            background: events[eventId].flag,
-          }}
-        />
         <a href={events[eventId].link}>
           <p>More info on Wikipedia</p>
         </a>
@@ -96,16 +95,36 @@ function App() {
             );
           })}
         </select>
+        <select
+          value={timezone}
+          onChange={(e) => {
+            setTimezone(e.currentTarget.value);
+            setEventDate(
+              dayjs(events[eventId].date).tz(e.currentTarget.value, true),
+            );
+          }}
+        >
+          {timezones.map((timeZ) => {
+            return (
+              <option value={timeZ.value} key={timeZ.value + timeZ.name}>
+                {timeZ.name}
+              </option>
+            );
+          })}
+        </select>
       </div>
       <footer
         style={{
-          height: 'fit-content',
           position: 'sticky',
           left: 0,
           bottom: 0,
+          height: '30vh',
+          width: '100%',
+          background: events[eventId].flag,
+          transition: 'background-color 2s',
         }}
       >
-        <img src="/crowd-1295674.svg" alt="crowd of protestors" />
+        {/* <img src="/crowd-1295674.svg" alt="crowd of protestors" /> */}
       </footer>
     </>
   );
